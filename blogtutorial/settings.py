1572237cd920f44e8fa8cde53b169284971487ee
Yvanmutara <37838552+Yvanmutara@users.noreply.github.com>
2023-07-18 18:49:06 +0200

    Update settings.py

--- blogtutorial/settings.py
+++ blogtutorial/settings.py
@@ -82,10 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangoblog',
-        'USER': 'postgres',
-        'PASSWORD': 'Admin',
+        'USER': 'admin',
+        'PASSWORD': 'testdjango123@',
         'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'PORT': ''
     }
 }
 
